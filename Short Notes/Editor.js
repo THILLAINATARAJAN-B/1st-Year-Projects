517b918603d4a30045c5531251895e6e4964a8aa
ThillaiNatarajan <133097043+THILLAINATARAJAN-B@users.noreply.github.com>
2023-06-03 09:24:35 +0530

    Add files via upload

---
+++ Short Notes/Editor.js
@@ -0,0 +1,18 @@
+let Title_1=''
+let TText_1=''
+function Save()
+{
+  Title_1=document.querySelector('.title_box').value;
+  console.log(Title_1);
+  TText_1=document.querySelector('.text_box').value;
+  console.log(TText_1);
+  localStorage.setItem(Title_1,TText_1);
+  console.log(localStorage);
+  document.querySelector('.title_box').value='';
+  document.querySelector('.text_box').value='';
+  window.history.back();
+};
+function Cancel()
+{
+  window.history.back();
+}
